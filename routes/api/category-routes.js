47c6add8cee771a3039a2c0f949059da59aeb434
Jeff Anderson-Lester <viperhq@hotmail.com>
2021-11-16 17:22:53 -0600

    completed category-routes and tested, no errors

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -23,18 +23,75 @@ router.get("/", (req, res) => {
 router.get("/:id", (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id,
+    },
+    include: [
+      {
+        model: Product,
+      },
+    ],
+  }).then((categoryData) => {
+    if (!categoryData) {
+      res.status(500).json({ message: "No Category with this ID" });
+      return;
+    }
+    res.json(categoryData);
+  });
 });
 
 router.post("/", (req, res) => {
   // create a new category
+  Category.create({
+    category_name: req.body.category_name,
+  })
+    .then((categoryData) => res.json(categoryData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.put("/:id", (req, res) => {
   // update a category by its `id` value
+  Category.update(
+    { category_name: req.body.category_name },
+    {
+      where: {
+        id: req.params.id,
+      },
+    }
+  )
+    .then((categoryData) => {
+      if (!categoryData) {
+        res.status(404).json({ message: "No Category Found" });
+        return;
+      }
+      res.json(categoryData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.delete("/:id", (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: { id: req.params.id },
+  })
+    .then((categoryData) => {
+      if (!categoryData) {
+        res.status(500).json({ message: "Nothing here" });
+        return;
+      }
+      res.json({ message: "Category deleted" });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
